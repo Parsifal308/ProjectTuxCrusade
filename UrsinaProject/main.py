2bcfdfdf35824ea8a615dfe30798673ccfcbbc9f
Parsifal308 <gyllestrato@gmail.com>
2021-06-04 05:22:12 -0300

    Proyecto Estructurado
    Se crearon directorios base del proyecto.
    
    ->Prefabs: Donde van todas las cosas listas para usar (musica, modelos, texturas, etc)
    
    ->Scripts: Van todos los archivos de codigo

--- UrsinaProject/main.py
+++ UrsinaProject/main.py
@@ -1,23 +1,24 @@
 from ursina import *
 
-app = Ursina()
 
-penguinTower = 'Tower Penguin.obj'
-tower = 'Tower 01.obj'
+#SE DEFINE LA APP
+tux_crusade = Ursina()
 
-player = Entity(model=penguinTower, color=color.blue, position=Vec3(0, -3, 0), scale_y=1)
-fichaUno = Entity(model=tower, color=color.red, position=Vec3(-1, 1, 0))
-fichaDos = Entity(model=tower, color=color.green, position=Vec3(-2, 2, 0))
-fichaTres = Entity(model=tower, color=color.pink, position=Vec3(1, -1, 0))
-fichaCuatro = Entity(model=tower, color=color.yellow, position=Vec3(2, -2, 0))
+#[DEFINICIONES]
+player = Entity(model='cube', color= color.red, position=Vec3(0, -3, 0), scale_y=1)
 
+#[METODO GLOBAL]
+#Se ejecuta en cada frame
 def update():
     player.x += held_keys['d'] * time.dt
     player.x -= held_keys['a'] * time.dt
 
+#[METODO GLOBAL]
+#Escucha entradas del teclado o mouse
 def input(key):
     if key == 'space':
         player.y += 1
         invoke(setattr, player, 'y', player.y-1, delay=.25)
 
-app.run()
+#SE EJECUTA LA APP
+tux_crusade.run()
